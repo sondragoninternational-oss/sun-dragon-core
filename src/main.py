de20ac21223180e4646590c9d6585145518dd3b3
sondragoninternational-oss <sondragon.international@gmail.com>
2026-04-14 00:18:11 +0200

    activate system entry point

---
+++ src/main.py
@@ -0,0 +1,8 @@
+from src.orchestrator.engine import Engine
+from src.services.pipeline import Pipeline
+
+if __name__ == '__main__':
+    e = Engine()
+    p = Pipeline()
+    print(e.run())
+    print(p.execute())
